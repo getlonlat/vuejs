18f6575a17d49f53e32b2591c306619b060e0a53
Éverton Inocêncio <hewerthomn@gmail.com>
2019-04-22 10:13:12 -0400

    last minute fixes.

---
+++ src/components/Notification.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="notification is-primary" v-if="showNotification">
+    <span class="delete" @click="hideNotification"></span>
+    Check the Leaflet version here:
+    <a href="https://getlonlat.github.io/vuejs-leaflet/">
+      https://getlonlat.github.io/vuejs-leaflet/
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    showNotification() {
+      return this.$store.getters.showNotification;
+    }
+  },
+  methods: {
+    hideNotification() {
+      this.$store.dispatch("hideNotification");
+    }
+  }
+};
+</script>
